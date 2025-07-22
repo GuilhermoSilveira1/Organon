209098d2c77cf02330abaf17873c799dde15a6ea
Guilhermo Silveira <guilhermosilveira@gmail.com>
2025-07-22 13:59:14 -0300

    Added the base.html to other html
    Added the base.html to the others html, so the pomodoro, task and home html.

--- organon/authentication/urls.py
+++ organon/authentication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 app_name = 'authentication'
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
+    path('logout/', LogoutView.as_view(next_page='authentication:login'), name='logout'),
 ]
--- organon/organon/urls.py
+++ organon/organon/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace='home')),
-    path('task/', include('task.urls', namespace='tasks')),
+    path('task/', include('task.urls', namespace='task')),
     path('auth/', include('authentication.urls', namespace='authentication')),
     path('pomodoro/', include('pomodoro.urls', namespace='pomodoro')),
     # path('reward/', include('reward.urls')),
